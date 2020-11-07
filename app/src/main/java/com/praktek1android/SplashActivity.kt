f8d54a8aa02a973a19800c38053266a60182c1cc
nur.azizah <azizahn346@gmail.com>
2020-11-08 01:41:57 +0800

    themes changed and make a delay run mainactivity

--- app/src/main/java/com/praktek1android/SplashActivity.kt
+++ app/src/main/java/com/praktek1android/SplashActivity.kt
@@ -1,11 +1,19 @@
 package com.praktek1android
 
+import android.content.intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        Handler(Looper.getMainLooper()).postDeLayed({
+            val i = Intent(packageContext: this, MainActivity::class.java)
+            startActivity(i)
+        }, delayMillis: 2000)
     }
 }
